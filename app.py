cef21639a0149d94dc067596e7fef110e41e848d
Vectors <programming@vectors.net.au>
2018-05-16 22:17:37 +0800

    added db call to planner

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+#! /usr/bin/env python3.5
+
 from flask import (Flask,
                    render_template,
-                   jsonify)
+                   jsonify,
+                   request,
+                   redirect)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base
+from database_setup import Base, Planner
+import httplib2
 
 app = Flask(__name__)
+
 
 engine = create_engine('sqlite:///controller.db')
 Base.metadata.bind = engine
@@ -24,11 +30,18 @@ def showTodo():
 
 @app.route("/planner")
 def showPlanner():
-    return render_template('planner.html')
+    arranger = session.query(Planner).all()
+    return render_template('planner.html', arranger=arranger)
+
+#JSON parser
+@app.route('/planner/JSON')
+def plannerJSON():
+    plans = session.query(Planner).all()
+    return jsonify(Plans=[i.serialize for i in plans])
 
 @app.route("/kb")
 def showKnowledgebase():
     return render_template('kb.html')
 
 if __name__ == '__main__':
-  app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
